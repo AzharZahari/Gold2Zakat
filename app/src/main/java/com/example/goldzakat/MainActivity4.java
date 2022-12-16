31bae38be6f41ae871fb7f8a914eb4c500c646e7
AzharZahari <azhar.aurum30@gmail.com>
2022-12-16 22:46:02 +0800

    Final Commit

--- app/src/main/java/com/example/goldzakat/MainActivity4.java
+++ app/src/main/java/com/example/goldzakat/MainActivity4.java
@@ -13,25 +13,19 @@ import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity4 extends AppCompatActivity {
 
-    ImageView studId, group, ws, link;
+    ImageView  ws, link;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main4);
 
-        studId = findViewById(R.id.studId);
-        group = findViewById(R.id.group);
+
         ws = findViewById(R.id.ws);
         link = findViewById(R.id.link);
 
 
-        studId.setOnClickListener(v -> Toast.makeText(getApplicationContext(),
-                "Student number",Toast.LENGTH_LONG).show());
 
-
-        group.setOnClickListener(v -> Toast.makeText(getApplicationContext(),
-                "Group",Toast.LENGTH_LONG).show());
 
         ws.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -43,7 +37,7 @@ public class MainActivity4 extends AppCompatActivity {
         link.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                gotoUrl("");
+                gotoUrl("https://github.com/AzharZahari/Gold2Zakat");
             }
         });
 
